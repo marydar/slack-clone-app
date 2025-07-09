a74d0a3fc8f8d96f2a30f9e9416352f782391606
Maryam Dar <maryamdar1384@gmail.com>
2025-07-09 22:40:40 +0330

    message toolbar

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -5,6 +5,7 @@ import { Hint } from "./hint";
 import { Button } from "./ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(() => import("@/components/renderer"),{ssr:false})
 const formatFullTime = (date:Date)=>{
@@ -108,6 +109,17 @@ export const Message = ({
                         ) : null }
                     </div>
                 </div> 
+                {!isEditing && (
+                    <Toolbar
+                        isAuthor={isAuthor}
+                        isPending={false}
+                        handleEdit={()=>{setEditingId(id)}}
+                        handleThread={()=>{}}
+                        handleDelete={()=>{}}
+                        handleReaction={()=>{}}
+                        hideThreadButton={hideThreadButton}
+                    />
+                )}
             </div>
     )
 }
